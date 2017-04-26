bfc8bec725959ea8d57ef84d3e5311d8565b2cc1
Andrew <awcurtin@gmail.com>
2017-04-26 15:47:09 -0400

    starting to add bindings

---
+++ DependencyInject/App_Start/Bindings.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DependencyInject.App_Start
+{
+    public class Bindings
+    {
+        public static void RegisterServices()
+        {
+            var builder = new ContainerBuilder();
+        }
+    }
+}
